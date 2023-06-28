2e32136f0893c6ac0e5614517690f2c427fa38a4
catani-maximiliano <Catanimaximiliano@gmail.com>
2023-06-28 13:01:15 -0300

    Login, Register, Logout, GetProducts, PostProducts,GetProductID, Profile, createCart,

--- src/controllers/auth/controller.auth.js
+++ src/controllers/auth/controller.auth.js
@@ -1,26 +1,29 @@
-const passport = require('passport');
+const passport = require("passport");
 const Route = require("../../router/Class.Router");
-const NodemailerAdapter = require('../../adapters/nodemailer.adapter');
+const NodemailerAdapter = require("../../adapters/nodemailer.adapter");
 const correo = new NodemailerAdapter();
-const { UserManager } = require('../../dao/mongoClassManagers/userClass/userMongoManager');
-const userBD = new UserManager();
-const { isValidPasswordMethod, createHash } = require('../../utils/cryptPassword');
 
-const { MongoCartManager } = require('../../dao/mongoClassManagers/cartsClass/cartMongoManager');
+const { UserManager} = require("../../dao/mongoClassManagers/userClass/userMongoManager");
+const userBD = new UserManager();
+
+const { isValidPasswordMethod, createHash} = require("../../utils/cryptPassword");
+
+const { MongoCartManager} = require("../../dao/mongoClassManagers/cartsClass/cartMongoManager");
 const cartsMongo = new MongoCartManager();
 
 class AuthRouter extends Route {
   init() {
 
-    this.post( "/", ["PUBLIC"], passport.authenticate("login", { failureRedirect: "/api/auth/failLogin" }),
+    //Login
+    this.post("/", ["PUBLIC"], passport.authenticate("login", { failureRedirect: "/api/auth/failLogin"}),
       async (req, res) => {
         try {
           if (!req.user) {
             return res.status(400).json({ error: "Credenciales inválidas" });
           } else {
-            console.log(req.user)
+            console.log(req.user);
 
-            req.session.destroy
+            req.session.destroy;
             req.session.user = {
               idd: req.user._id,
               first_name: req.user.first_name,
@@ -30,13 +33,14 @@ class AuthRouter extends Route {
               role: "usuario",
               last_connection: req.user.last_connection,
             };
-            
-            
+
+            cartsMongo.addCart(req.user._id, {
+              user_id: req.user._id,
+              products: {},
+            });
 
             const dateNow = new Date();
             await userBD.updateConnection(req.user.email, dateNow);
-            
-                  cartsMongo.addCart({user_id: req.user._id,products:{}});
 
             res.sendSuccess(req.user);
           }
@@ -46,26 +50,30 @@ class AuthRouter extends Route {
       }
     );
 
-    this.post('/passwordReset', ['PUBLIC'], async (req, res) => {
+    //Reset de Contraseña
+    this.post("/passwordReset", ["PUBLIC"], async (req, res) => {
       try {
         const expirationTime = new Date().getTime() + 3600000;
-        let linkMold = req.protocol + '://' + req.get('host');
+        let linkMold = req.protocol + "://" + req.get("host");
         const url = linkMold + `/passwordReset/${expirationTime}`;
-        const email = { email: req.body.user }
-        req.session.destroy
+        const email = { email: req.body.user };
+        req.session.destroy;
         req.session.expirationTime = expirationTime;
-        req.session.email = email
-        const mensaje = { message: `<div> <h1>Hola!</h1> <h2>Este es el link para recuperar tu contreseña</h2> <h3> ${url}</h3> </div>`, subject: 'Recuperacion  de contraseña' }
+        req.session.email = email;
+        const mensaje = {
+          message: `<div> <h1>Hola!</h1> <h2>Este es el link para recuperar tu contreseña</h2> <h3> ${url}</h3> </div>`,
+          subject: "Recuperacion  de contraseña",
+        };
         const emailSend = await correo.sendNotification(email, mensaje);
-        console.log(emailSend)
+        console.log(emailSend);
         res.json({ emailSend });
+      } catch (error) {
+        res.sendServerError(`something went wrong ${error}`);
       }
-      catch (error) {
-        res.sendServerError(`something went wrong ${error}`)
-      }
-    })
+    });
 
-    this.post('/passwordUpdate', ['PUBLIC'], async (req, res) => {
+    // actulizacion de contraseña
+    this.post("/passwordUpdate", ["PUBLIC"], async (req, res) => {
       try {
         const pw1 = req.body.newPassword1;
         const pw2 = req.body.newPassword2;
@@ -74,84 +82,93 @@ class AuthRouter extends Route {
         if (pw1 === pw2) {
           if (isValidPasswordMethod(pw1, user)) {
             console.log("contraseña igual a la anterior");
-            res.json({ mesagge: 'Contraseña igual a la anterior, usar una nueva.' });
-          }
-          else {
+            res.json({
+              mesagge: "Contraseña igual a la anterior, usar una nueva.",
+            });
+          } else {
             await userBD.updatePassword(email, createHash(pw1));
-            res.json({ mesagge: 'Contraseña actualizada' });
+            res.json({ mesagge: "Contraseña actualizada" });
           }
+        } else {
+          res.json({ mesagge: "Contraseñas no coinciden." });
         }
-        else {
-          res.json({ mesagge: 'Contraseñas no coinciden.' })
-        }
+      } catch (error) {
+        res.sendServerError(`something went wrong ${error}`);
       }
-      catch (error) {
-        res.sendServerError(`something went wrong ${error}`)
-      }
-    })
+    });
 
-    this.get('/failLogin', ['PUBLIC'], (req, res) => {
+    //fallo de Login
+    this.get("/failLogin", ["PUBLIC"], (req, res) => {
       try {
-        res.json({ error: 'Falló el login' });
+        res.json({ error: "Falló el login" });
+      } catch (error) {
+        res.sendServerError(`something went wrong ${error}`);
       }
-      catch (error) {
-        res.sendServerError(`something went wrong ${error}`)
+    });
+
+    //LogOut
+    this.get("/logout", ["PUBLIC"], (req, res) => {
+      try {
+        req.session.destroy((err) => {
+          if (err) {
+            res.json({ msg: err });
+          }
+          res.redirect("/login");
+        });
+      } catch (error) {
+        res.sendServerError(`something went wrong ${error}`);
       }
-    })
+    });
+    
 
-    this.get('/github', ['PUBLIC'],
-      passport.authenticate('github', { scope: ['user:email'] }),
-      async (req, res) => { }
-    )
+    
+    this.get(
+      "/github",
+      ["PUBLIC"],
+      passport.authenticate("github", { scope: ["user:email"] }),
+      async (req, res) => {}
+    );
 
-    this.get('/githubcallback', ['PUBLIC'],
-      passport.authenticate('github', { failureRedirect: '/login' }),
-      async (req, res) => {
-        try {
-          req.session.user = req.user;
-          const fechaActual = new Date();
-        await userBD.updateConnection(req.user.email, fechaActual);
-          res.redirect('/products');
-        }
-        catch (error) {
-          res.sendServerError(`something went wrong ${error}`)
-        }
-      }
-    )
-
-    this.get('/google', ['PUBLIC'],
-      passport.authenticate('google', { scope: ['profile'] }),
-      async (req, res) => { }
-    )
-
-    this.get('/google/callback', ['PUBLIC'],
-      passport.authenticate('google', { failureRedirect: '/login' }),
+    this.get(
+      "/githubcallback",
+      ["PUBLIC"],
+      passport.authenticate("github", { failureRedirect: "/login" }),
       async (req, res) => {
         try {
           req.session.user = req.user;
           const fechaActual = new Date();
           await userBD.updateConnection(req.user.email, fechaActual);
-          res.redirect('/products');
-        }
-        catch (error) {
-          res.sendServerError(`something went wrong ${error}`)
+          res.redirect("/products");
+        } catch (error) {
+          res.sendServerError(`something went wrong ${error}`);
         }
       }
-    )
+    );
 
-    this.get('/logout', ['PUBLIC'], (req, res) => {
-      try {
-        req.session.destroy(err => {
-          if (err) {
-            res.json({ msg: err })
-          }
-          res.redirect("/login");
-        })
+    this.get(
+      "/google",
+      ["PUBLIC"],
+      passport.authenticate("google", { scope: ["profile"] }),
+      async (req, res) => {}
+    );
+
+    this.get(
+      "/google/callback",
+      ["PUBLIC"],
+      passport.authenticate("google", { failureRedirect: "/login" }),
+      async (req, res) => {
+        try {
+          req.session.user = req.user;
+          const fechaActual = new Date();
+          await userBD.updateConnection(req.user.email, fechaActual);
+          res.redirect("/products");
+        } catch (error) {
+          res.sendServerError(`something went wrong ${error}`);
+        }
       }
-      catch (error) {
-        res.sendServerError(`something went wrong ${error}`)
-      }
-    })
+    );
+
+
   }
 }
 
--- src/dao/mongoClassManagers/cartsClass/cartMongoManager.js
+++ src/dao/mongoClassManagers/cartsClass/cartMongoManager.js
@@ -1,10 +1,7 @@
 const Cart = require("../../models/carts.model");
 
 class MongoCartManager {
-  /**La función getCarts es una función asincrónica que se encarga de obtener todos los documentos de la colección
-   *  carts de la base de datos y devolverlos en un array. Para ello, se utiliza el método find() de Mongoose que
-   * busca en la colección todos los documentos que cumplen ciertas condiciones y los devuelve en un array.
-   *  En caso de que ocurra algún error, se maneja en el bloque catch y se devuelve el error. */
+
   async getCarts() {
     try {
       const carts = await Cart.find();
@@ -13,57 +10,48 @@ class MongoCartManager {
       return error;
     }
   }
-  /**Este método addCart() utiliza el método create() de Mongoose para agregar un nuevo carrito a la colección de carritos.
- *  Recibe un objeto cart que contiene una lista de productos con sus respectivas cantidades.
 
-Primero, intenta crear un nuevo documento de carrito usando Cart.create(cart). Si se realiza con éxito, devuelve un mensaje
- de éxito indicando que el carrito se agregó correctamente. De lo contrario, captura cualquier error y lo devuelve. */
-  async addCart(cart) {
-    try {console.log("carttt")
-       await Cart.create(cart);
-       console.log(cart)
-      return "Cart added successfully";
+  async addCart(uid, cart) {
+    try {
+      const getCartByUIdMongo = await Cart.findOne({ user_id: uid });
+      if (!getCartByUIdMongo) {
+        await Cart.create(cart);
+        console.log(cart);
+        return "Cart added successfully";
+      }
     } catch (error) {
       return error;
     }
   }
 
-  /**
-La función getCartById sirve para buscar un carrito en particular en la base de datos a partir de su identificador único (_id).
-
-Primero se intenta encontrar el carrito en la base de datos utilizando el método findOne() de Mongoose, pasando como argumento 
-un objeto con la propiedad _id igual al valor de id. Si se encuentra el carrito, se devuelve el objeto correspondiente. 
-Si no se encuentra, se devuelve el valor null. */
   async getCartById(id) {
     try {
-      const getCartByIdMongo = await Cart.findOne({ _id: id });
+      const getCartByIdMongo = Cart.findOne({ _id: id });
       return getCartByIdMongo;
     } catch (error) {
       return error;
     }
   }
 
-  async getCartByUId(uid) {
+  async getCartByUId(_id) {
     try {
-      const getCartByUIdMongo = await Cart.findOne({ user_id: uid });
+      const getCartByUIdMongo = await Cart.findOne({ user_id: _id });
+      console.log(getCartByUIdMongo);
       return getCartByUIdMongo;
     } catch (error) {
       return error;
     }
   }
 
-  /**
-Esta función se utiliza para agregar un producto a un carrito existente en la base de datos. Toma como parámetros el id
- del carrito, el id del producto y un valor booleano que indica si el producto ya está en el carrito o no. Si el producto
-  ya está en el carrito, se incrementa la cantidad del producto en uno, de lo contrario, se agrega el producto al carrito
-   con una cantidad de uno. Luego se actualiza el documento del carrito en la base de datos con los cambios realizados.
-    La función devuelve la respuesta de la base de datos después de actualizar el documento del carrito. */
   async postCartProductsId(idCart, idProduct, exist) {
     try {
       const cart = await Cart.findById(idCart);
       if (exist) {
-        const productsArrayPosition = cart.products.findIndex((item) => item.product.id == idProduct);
-        cart.products[productsArrayPosition].quantity = cart.products[productsArrayPosition].quantity + 1;
+        const productsArrayPosition = cart.products.findIndex(
+          (item) => item.product.id == idProduct
+        );
+        cart.products[productsArrayPosition].quantity =
+          cart.products[productsArrayPosition].quantity + 1;
       } else {
         cart.products.push({ product: idProduct, quantity: 1 });
       }
@@ -74,12 +62,6 @@ Esta función se utiliza para agregar un producto a un carrito existente en la b
     }
   }
 
-  /**Esta función permite eliminar un conjunto de productos de un carrito específico. Recibe como parámetros el id
-     *  del carrito que se quiere modificar y el nuevo array de productos que se quiere guardar en el carrito actualizado.
-
-Primero se utiliza el método findByIdAndUpdate() de Mongoose para buscar el carrito por su id y actualizar su campo products
- con el nuevo array de productos. Si todo sale bien, la función devuelve un mensaje de éxito indicando que los productos se
-  eliminaron del carrito. En caso de error, se devuelve el error correspondiente. */
   async deleteCartProductsId(id, arrayProducts) {
     try {
       const ProductByIdMongo = await Cart.findByIdAndUpdate(id, {
@@ -91,10 +73,6 @@ Primero se utiliza el método findByIdAndUpdate() de Mongoose para buscar el car
     }
   }
 
-  /**
-Esta función se utiliza para eliminar un carrito de compras por su ID. Primero, utiliza el método findByIdAndDelete de Mongoose
- para buscar el carrito por su ID y eliminarlo de la base de datos. Luego, devuelve un mensaje indicando que el carrito 
- se ha eliminado correctamente o devuelve un mensaje de error en caso contrario. */
   async deleteById(id) {
     try {
       const deleteByIdMongo = await Cart.findByIdAndDelete(id);
@@ -108,14 +86,15 @@ Esta función se utiliza para eliminar un carrito de compras por su ID. Primero,
      *  Recibe como parámetros el ID del carrito (idCart), el ID del producto (idProduct), una bandera booleana exist que 
      * indica si el producto ya existe en el carrito o no, y la cantidad deseada (quantity).
 
-Si exist es true, la función buscará la posición del producto en el arreglo de productos del carrito (cart.products) mediante 
-el ID del producto y actualizará la cantidad con la cantidad deseada. En caso contrario, se agregará un nuevo objeto al 
-arreglo de productos con el ID del producto y la cantidad deseada.
+      Si exist es true, la función buscará la posición del producto en el arreglo de productos del carrito (cart.products) mediante 
+      el ID del producto y actualizará la cantidad con la cantidad deseada. En caso contrario, se agregará un nuevo objeto al 
+      arreglo de productos con el ID del producto y la cantidad deseada.
 
-Luego, la función utiliza findByIdAndUpdate para actualizar el documento del carrito con los nuevos valores del arreglo de 
-productos y devuelve el resultado de la operación. */
+      Luego, la función utiliza findByIdAndUpdate para actualizar el documento del carrito con los nuevos valores del arreglo de 
+      productos y devuelve el resultado de la operación. */
   async updateCartProductsId(idCart, idProduct, exist, quantity) {
     try {
+      //const cart = await Cart.findByUId(idCart);
       const cart = await Cart.findById(idCart);
       if (exist) {
         const productsArrayPosition = cart.products.findIndex(
