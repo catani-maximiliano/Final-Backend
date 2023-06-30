9b167e5926cdee262a5d4ddeea838d1845734448
catani-maximiliano <Catanimaximiliano@gmail.com>
2023-06-30 17:41:48 -0300

    .

--- src/utils/errors/CustomErrors.js
+++ src/utils/errors/CustomErrors.js
@@ -1,7 +1,4 @@
-/**La clase CustomError tiene un método estático llamado createError que recibe un objeto con cuatro 
- * propiedades opcionales: name, cause, message, y code. El método crea una nueva instancia de la clase 
- * Error con el mensaje pasado en la propiedad message, y asigna las propiedades name, cause, y code a las 
- * correspondientes pasadas en el objeto. */
+
 class CustomError{
     static createError = ({name = "Error", cause, message, code = 1}) => {
         const error = new Error(message);
--- src/utils/errors/product/product.error.js
+++ src/utils/errors/product/product.error.js
@@ -4,16 +4,6 @@ const enumErrors = require("../enumErrors")
 
 require("colors")
 
-/**Este es un ejemplo de una función llamada productError que se encarga de verificar si los datos de un 
- * producto son válidos antes de agregarlo a la base de datos. La función toma dos parámetros, pid que es el 
- * ID del producto y obj que es un objeto que contiene los datos del producto.
-
-En la función, se verifica si alguno de los campos del objeto son nulos o indefinidos. Si se encuentra alguno 
-de estos casos, se crea un error personalizado utilizando una librería llamada CustomError. El error personalizado 
-contiene información detallada sobre el error que se produjo, incluyendo el campo que no es válido y el tipo esperado.
-
-Este tipo de validación es útil para garantizar la integridad de los datos almacenados en la base de datos y 
-prevenir errores posteriores debido a datos incorrectos o incompletos. */
 const productError = (pid, obj) => {
 
     if(!obj.title || !obj.description || !obj.price || !obj.thumbail || !obj.code || !obj.stock || !obj.category){
